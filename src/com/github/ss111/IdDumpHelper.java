52c8ebd344427215077c276e4cb26d0c15436472
Sam Mauck <smmauck@gmail.com>
2013-08-12 14:23:38 -0600

    Don't always dispose of window

--- src/com/github/ss111/IdDumpHelper.java
+++ src/com/github/ss111/IdDumpHelper.java
@@ -107,10 +107,10 @@ public class IdDumpHelper {
 		if (unusedItemIDs.size() == 0) {
 			
 			JOptionPane.showMessageDialog(null, "There are no unused item IDs. Are you sure you dumped them? The program cannot continue and will now close.", "Information", JOptionPane.ERROR_MESSAGE);
+			
+			WindowMain.frmMain.dispose();
 		}
-		
-		WindowMain.frmMain.dispose();
-		
+				
 		return unusedItemIDs;
 	}
 
