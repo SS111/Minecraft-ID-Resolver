0e0af01a45561e219c1c011a4cd40317514ce5cc
Sam Mauck <smmauck@gmail.com>
2014-07-13 09:57:22 -0600

    Allow conflicts in the same config file. Fixes #6

--- src/com/github/ss111/ConflictHelper.java
+++ src/com/github/ss111/ConflictHelper.java
@@ -40,15 +40,13 @@ public class ConflictHelper {
 		} else {
 			
 			ArrayList<String> namesArray = new ArrayList<String>();
-			ArrayList<String> configArray = new ArrayList<String>();
 			
 			for (ArrayList<String> combo : names) {
 				
 				namesArray.add(combo.toArray()[0].toString());
-				configArray.add(combo.toArray()[1].toString());
 			}
 			
-			if (namesArray.toArray()[0].toString().equals(namesArray.toArray()[1].toString()) || configArray.toArray()[0].toString().equals(configArray.toArray()[1].toString())) {
+			if (namesArray.toArray()[0].toString().equals(namesArray.toArray()[1].toString())) {
 				
 				return false;
 				
